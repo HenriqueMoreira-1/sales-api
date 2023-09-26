8ae40caca2e104e9c849812f291483d66a547397
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-09-26 10:24:56 -0300

    feat: config server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,12 @@
-console.log('hello dev')
+import express from "express";
+import cors from 'cors'
+import routes from "./routes";
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3333, () => console.log("Server is running!"))

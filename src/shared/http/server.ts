ef49bb31b26470bcd9a8896fea8f1c187179879f
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-09-26 15:16:00 -0300

    feat: middleware to handle error

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express"
 import cors from 'cors'
-import routes from "./routes";
+import routes from "./routes"
+import AppError from "@shared/errors/AppError"
 
 const app = express()
 
@@ -8,5 +9,19 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      error: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3333, () => console.log("Server is running!"))

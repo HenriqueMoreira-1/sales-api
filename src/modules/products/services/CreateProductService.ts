8dd4443e3bd72e35061c661e7be896076dad7ecd
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-10-06 10:07:54 -0300

    feat: CreateProductService

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from "typeorm";
+import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+import AppError from "@shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+    public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+      const productsRepository = getCustomRepository(ProductsRepository)
+      const productExists = await productsRepository.findByName(name);
+
+        if (productExists) {
+            throw new AppError('There is already one product with this name');
+        }
+
+        const product = productsRepository.create({
+            name,
+            price,
+            quantity
+        });
+        await productsRepository.save(product);
+        return product;
+    }
+}
+
+export default CreateProductService;
